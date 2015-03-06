9f353b28f9cd7f90a25189c068df3187edb72cca
Arne (Virtual Win) <aleitert@kent.edu>
2015-03-06 12:01:03 -0500

    Changed concept for matrix colors.

--- HypergraphProject/Interface/MatrixControl/MatrixColors.cs
+++ HypergraphProject/Interface/MatrixControl/MatrixColors.cs
@@ -7,62 +7,108 @@ namespace HypergraphProject.Interface
     class MatrixColors
     {
 
-        private Color[, , ,] colorSet;
+        private Color[, ,] fieldColorSet;
+        private Color[, ,] bgColorSet;
 
         public MatrixColors()
         {
-            // Zero/One, EditMode, ColorFunction, Mouse
-            colorSet = new Color[2, 3, 3, 2];
+            // Zero/One, EditMode, ColorFunction
+            fieldColorSet = new Color[2, 3, 3];
 
             // ----------
             // 0 - Fixed
 
-            this[false, EditMode.Fixed, ColorFunction.Background, false] = Color.White;
-            this[false, EditMode.Fixed, ColorFunction.Background, true] = Color.LightYellow;
-
-            this[false, EditMode.Fixed, ColorFunction.Border, false] = Color.Transparent;
-            this[false, EditMode.Fixed, ColorFunction.Border, true] = Color.Transparent;
-
-            this[false, EditMode.Fixed, ColorFunction.Text, false] = Color.Gray;
-            this[false, EditMode.Fixed, ColorFunction.Text, true] = Color.Gray;
+            this[false, EditMode.Fixed, ColorFunction.Background] = Color.Transparent;
+            this[false, EditMode.Fixed, ColorFunction.Border] = Color.Transparent;
+            this[false, EditMode.Fixed, ColorFunction.Text] = Color.Gray;
 
 
             // ----------
             // 1 - Fixed
 
-            this[true, EditMode.Fixed, ColorFunction.Background, false] = Color.FromArgb(0x8DB3E2);
-            this[true, EditMode.Fixed, ColorFunction.Background, true] = Color.FromArgb(0x8DB3E2);
+            this[true, EditMode.Fixed, ColorFunction.Background] = Color.FromArgb(0x8DB3E2);
+            this[true, EditMode.Fixed, ColorFunction.Border] = Color.FromArgb(0x1F497D);
+            this[true, EditMode.Fixed, ColorFunction.Text] = Color.Black;
 
-            this[true, EditMode.Fixed, ColorFunction.Border, false] = Color.FromArgb(0x1F497D);
-            this[true, EditMode.Fixed, ColorFunction.Border, true] = Color.FromArgb(0x1F497D);
-                 
-            this[true, EditMode.Fixed, ColorFunction.Text, false] = Color.Black;
-            this[true, EditMode.Fixed, ColorFunction.Text, true] = Color.Black;
+
+            // ------------------------------
+
+
+            // Matrix EditMode, Row EditMode, Mouse
+            bgColorSet = new Color[3, 3, 2];
+
+            this[EditMode.Fixed, EditMode.Fixed, false] = Color.Transparent;
+            this[EditMode.Fixed, EditMode.Fixed, true] = Color.LightYellow;
 
             // ToDo: Remaining colors.
 
         }
 
-        public Color this[bool isOne, EditMode editMode, ColorFunction function, bool mouseHover]
+        /// <summary>
+        /// Returns the color of a field in the matrix.
+        /// </summary>
+        /// <param name="isOne">
+        /// Is the field set to 1.
+        /// </param>
+        /// <param name="editMode">
+        /// The edit mode of this field, i.e. of this row or column.
+        /// </param>
+        /// <param name="function">
+        /// What will be drawn with this color.
+        /// </param>
+        /// <returns></returns>
+        public Color this[bool isOne, EditMode editMode, ColorFunction function]
         {
             get
             {
-                return colorSet[
+                return fieldColorSet[
                     isOne ? 1 : 0,
                     (int)editMode,
-                    (int)function,
+                    (int)function
+                ];
+            }
+            set
+            {
+                fieldColorSet[
+                    isOne ? 1 : 0,
+                    (int)editMode,
+                    (int)function
+                ] = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns the background color for rows and colums.
+        /// </summary>
+        /// <param name="matrixEditMode">
+        /// The global edit mode.
+        /// </param>
+        /// <param name="rowEditMode">
+        /// The edit mode of this field, i.e. of this row or column.
+        /// </param>
+        /// <param name="mouseHover">
+        /// Is the mouse over this row or column.
+        /// </param>
+        /// <returns></returns>
+        public Color this[EditMode matrixEditMode, EditMode rowEditMode, bool mouseHover]
+        {
+            get
+            {
+                return bgColorSet[
+                    (int)matrixEditMode,
+                    (int)rowEditMode,
                     mouseHover ? 1 : 0
                 ];
             }
             set
             {
-                colorSet[
-                    isOne ? 1 : 0,
-                    (int)editMode,
-                    (int)function,
+                bgColorSet[
+                    (int)matrixEditMode,
+                    (int)rowEditMode,
                     mouseHover ? 1 : 0
                 ] = value;
             }
         }
+
     }
 }
